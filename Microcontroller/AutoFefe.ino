7ce517230ae00d3ba9ba3060223b07f597bf45e9
Luisão <Luisao.official@gmail.com>
2023-11-14 18:06:58 -0300

    Updated the arduino sketch to the debounced version

--- Microcontroller/AutoFefe.ino
+++ Microcontroller/AutoFefe.ino
@@ -1,76 +1,95 @@
-// Define the pin for low state detection
-const int actionPin = 2;
-const int leftSwitch = 4;
-const int rightSwitch = 7;
-boolean actionDetected = false;
+const int numOfInputs = 4;
+const int InputPins[numOfInputs] = {2, 4, 7, 8};
+int InputStates[numOfInputs] = {0, 0, 0, 0};
+int InputStatesOld[numOfInputs] = {0, 0, 0, 0};
+bool inputFlags[numOfInputs] = {false, false, false, false};
+long LastDebounceTime[numOfInputs] = {0, 0, 0, 0};
+long DebounceDelay = 10;
 
-const char* commands[] = {
-  "checkEvents",
-  "testInternet",
-  "sendMail"
-};
-
-int currentCommandIndex = 0;
+const int numOfTasks = 3;
+int currentTask = 0;
+String tasks[numOfTasks] = {"checkEvents", "testInternet", "sendMail"};
 
 void setup()
 {
-  // Initialize Serial communication with .1 timeout
+  for (int i = 0; i < numOfInputs; i++)
+  {
+    pinMode(InputPins[i], INPUT_PULLUP);
+  }
   Serial.begin(115200);
-  Serial.setTimeout(10);
-  pinMode(actionPin, INPUT_PULLUP); // Enable internal pull-up resistor
-  pinMode(rightSwitch, INPUT_PULLUP);
-  pinMode(leftSwitch, INPUT_PULLUP);
-
 }
 
-// Define the pin for low state detection
+// ! Debouncing
 
-void loop() {
-  if (!actionDetected && digitalRead(actionPin) == HIGH) {
-    actionDetected = true; // Set the actionDetected flag
-    
-    // Action detected, send the "time" command
-    // Serial.println("events");
-    Serial.println("pressedHome");
-    while (actionPin != HIGH)
-    {
-      delay(5);
-    }.
-    
-    
-    do {
-      if (digitalRead(rightSwitch) == LOW) {
-        currentCommandIndex = (currentCommandIndex + 1) % (sizeof(commands) / sizeof(commands[0]));
-        Serial.println("right");
-        Serial.println(commands[currentCommandIndex]);
-      } else if (digitalRead(leftSwitch) == LOW) {
-        currentCommandIndex = (currentCommandIndex - 1 + (sizeof(commands) / sizeof(commands[0]))) % (sizeof(commands) / sizeof(commands[0]));
-        Serial.println("pressedLeft");
-        Serial.println(commands[currentCommandIndex]);
-      }
-      
-      // Consider using millis() for non-blocking timing
-      delay(1000);
-      
-    } while (digitalRead(actionPin) == HIGH);
-    
-    // Print the final selected command
-    Serial.print("execute.");
-    Serial.println(commands[currentCommandIndex]);
-  }
-
-
-  
-
-  if (actionDetected)
+void setInputFlags()
+{
+  for (int i = 0; i < numOfInputs; i++)
   {
-    // Wait for a response from Python
-    if (Serial.available() > 0)
+    int reading = digitalRead(InputPins[i]);
+    if (reading != InputStatesOld[i])
     {
-      String response = Serial.readStringUntil('\n');
-      // Process the response here
-      // Serial.println("Received: " + response);
-      actionDetected = false; // Reset the flag
+      LastDebounceTime[i] = millis();
+    }
+    if ((millis() - LastDebounceTime[i]) > DebounceDelay)
+    {
+      if (InputStates[i] != reading)
+      {
+        InputStates[i] = reading;
+        if (InputStates[i] == LOW)
+        {
+          inputFlags[i] = true;
+        }
+      }
+    }
+    InputStatesOld[i] = reading;
+  }
+}
+
+void resolveInputFlags()
+{
+  for (int i = 0; i < numOfInputs; i++)
+  {
+    if (inputFlags[i] == true)
+    {
+      Serial.println("pressed");
+      Serial.println(i);
+      inputAction(i);
+      inputFlags[i] = false;
     }
   }
+}
+
+void inputAction(int input)
+{
+  if (input == 0)
+  {
+    Serial.println("pressedHome");
+    Serial.println(tasks[currentTask]);
+    currentTask = 0;
+  }
+  else if (input == 1)
+  {
+    Serial.println("pressedLeft");
+    currentTask = (currentTask - 1 + numOfTasks) % numOfTasks;
+    Serial.print("currentTask.");
+    Serial.println(tasks[currentTask]);
+  }
+  else if (input == 2)
+  {
+    Serial.println("pressedRight");
+    currentTask = (currentTask + 1) % numOfTasks;
+    Serial.print("currentTask.");
+    Serial.println(tasks[currentTask]);
+  }
+  else if (input == 3)
+  {
+    Serial.print("execute.");
+    Serial.println(tasks[currentTask]);
+  }
+}
+
+void loop()
+{
+  setInputFlags();
+  resolveInputFlags();
 }
